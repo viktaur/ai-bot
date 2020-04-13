be62e5121246c2938a477f0168e41156e9917a94
viktaur <victorgabal@gmail.com>
2020-04-14 00:19:51 +0200

    u

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import nltk
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
-import numpy
+import numpy as np
 import tflearn
 import tensorflow
 import random
@@ -11,4 +11,47 @@ import json
 with open("intents.json") as file:
 	data = json.load(file)
 
-print(data)
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data["intents"]:
+	for patterns in intent["pattens"]:
+		wrds = nltk.word_tokenize(pattern)
+		words.extend(wrds)
+		docs_x.append(pattern)
+		docs_y.append(intent["tag"])
+
+		if intent["tag"] not in labels:
+			labels.append(intent["tag"])
+
+words = [stemmer.stem(w.lower()) for w in words]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(classes))]
+
+for x, doc in enumerate(docs_x):
+	bag = []
+
+	wrds = [stemmer.stem(w) for w in in doc]
+
+	for w in words:
+		if w in wrds:
+			bag.append(1)
+		else:
+			bag.append(0)
+
+	output_row = out_empty[:]
+	output_row[classes.index(docs_y[x])] = 1
+
+	training.append(bag)
+	output.append(output_row)
+
+training = np.array(training)
+output = np.array(output)
